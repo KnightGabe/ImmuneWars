b7bf5040056a5e87d3ae23033d13352d43e60214
Gabriel Maia <gmaia71@gmail.com>
2018-02-19 20:01:10 -0300

    Movimento, agora com os assets que estavam faltando no commit anterior

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+	Rigidbody rb;
+
+	[SerializeField]
+	float turnSpeed;
+	[SerializeField]
+	float forwardSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Turn();
+		Move();
+	}
+
+	void Move()
+	{
+		float thrust = Input.GetAxis("Vertical");
+		rb.velocity = (transform.forward * thrust).normalized * forwardSpeed;
+	}
+
+	void Turn()
+	{
+		float rotateY = Input.GetAxis("Mouse X");
+		float rotateX = Input.GetAxis("Mouse Y");
+		float rotateZ = Input.GetAxis("Horizontal");
+
+		Vector3 rotation = new Vector3(rotateX, rotateY, rotateZ) * turnSpeed;
+
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+	}
+}
