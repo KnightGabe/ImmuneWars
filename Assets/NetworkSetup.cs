0c6ad7b960f75f34844d2e41f4d2bc8dcd9b9b8c
Gabriel Maia <gmaia71@gmail.com>
2018-02-24 18:22:31 -0300

    Networking OK

---
+++ Assets/NetworkSetup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSetup :  NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] disableObjects;
+	
+	Camera lobbyCamera;
+
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer)
+		{
+			for (int i = 0; i < disableObjects.Length; i++)
+			{
+				disableObjects[i].enabled = false;
+			}
+		}
+		else
+		{
+			lobbyCamera = Camera.main;
+			if(lobbyCamera != null)
+			{
+				lobbyCamera.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	private void OnDisable()
+	{
+		if(lobbyCamera != null)
+		{
+			lobbyCamera.gameObject.SetActive(true);
+		}
+	}
+}
