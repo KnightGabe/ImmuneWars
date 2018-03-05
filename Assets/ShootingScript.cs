85d58727151c5657e8cd5ff6685656cad5a987e9
Flantsune <Flantsune@users.noreply.github.com>
2018-03-05 11:28:10 -0300

    FUCK OFF, LET MY TRAILS ALONE

---
+++ Assets/ShootingScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShootingScript : NetworkBehaviour {
+
+	public GameObject tiro;
+
+	public Transform tiroPos;
+
+	public ShipMovement player;
+
+	public float SpeedBullet;
+	public float TimerBullet;
+	public float RangeBullet;
+	public float CooldownBullet;
+
+	public int DamageBullet;
+
+	public bool canFire;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponentInChildren<ShipMovement>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButton(0) && canFire || Input.GetKey(KeyCode.Space) && canFire)
+		{
+			CmdProjectileShoot();
+		}
+		TimerBullet += Time.deltaTime;
+		if (TimerBullet >= CooldownBullet)
+		{
+			canFire = true;
+		}
+	}
+
+	[Command]
+	void CmdProjectileShoot()
+	{
+		if (isLocalPlayer)
+		{
+			GameObject nTiro = Instantiate(tiro, tiroPos.position, Quaternion.identity);
+			NetworkServer.SpawnWithClientAuthority(nTiro, gameObject);
+			RpcAddSpeed(nTiro);
+		}
+	}
+
+	[ClientRpc]
+	void RpcAddSpeed(GameObject tiro)
+	{
+		tiro.layer = LayerMask.NameToLayer("PlayerLocal");
+		tiro.GetComponent<Rigidbody>().velocity = player.gameObject.transform.forward * SpeedBullet;
+		canFire = false;
+		TimerBullet = 0;
+	}
+
+	void HitscanShoot()
+	{
+		RaycastHit hit;
+		Physics.Raycast(transform.position, transform.forward, out hit, RangeBullet, LayerMask.NameToLayer("PlayerLocal"));
+		if (hit.collider.gameObject.layer == LayerMask.NameToLayer("PlayerRemoto"))
+		{
+			hit.collider.gameObject.GetComponent<ShipMovement>().TakeDamage(DamageBullet);
+		}
+		canFire = false;
+		TimerBullet = 0;
+	}
+}
